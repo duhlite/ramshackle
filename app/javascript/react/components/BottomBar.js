4712c0c30b1206e466dd87b7e29de06e6deee775
tylerhuimingalone <tylermingalone@gmail.com>
2021-07-24 15:29:47 -0400

    updated styling and bottom bar

---
+++ app/javascript/react/components/BottomBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const BottomBar = () => {
+  return (
+    <div id="bottom-bar">
+      <p>Questions or Inquiries:</p>
+      <a href = "mailto: ramshacklefarms@gmail.com">Contact Us</a>
+    </div>
+  )
+}
+
+export default BottomBar
